629e53ab5b87ade7ee2cf7fe18130011ca87c038
Misyel <ydw317@naver.com>
2025-09-13 14:27:09 +0900

    undefined (reading 'thumbnailUrl') 수정 중

--- cake-quake-front/src/components/cake/itemComponents/cakeDetailComponent.jsx
+++ cake-quake-front/src/components/cake/itemComponents/cakeDetailComponent.jsx
@@ -82,8 +82,8 @@ function CakeDetailComponent({
 
     const currentShopName = shop?.shopName || '매장 정보 없음';
     const shopAddress = shop?.address || '주소 정보 없음';
-    const shopThumbnail = shop.thumbnailUrl
-        ? `${S3_BASE_URL}${shop.thumbnailUrl}`
+    const shopThumbnail = shop?.thumbnailUrl
+        ? `${S3_BASE_URL}upload/${shop.thumbnailUrl}`
         : DEFAULT_SHOP_THUMBNAIL;
 
     return (
